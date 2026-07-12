b5f0c61d5dfb719d72a140fab43f4dbcce6b7e5c
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-12 07:56:29 +0530

    Create a Dataframe(city,population State)

---
+++ 4_problem.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    "City": ["Indore","Ujjain","Bhopal"],
+    "Population": [11000,8000,7000],
+    "State": ["Mp","Mp","Mp"]
+}
+
+df = pd.DataFrame(data)
+print(df)
