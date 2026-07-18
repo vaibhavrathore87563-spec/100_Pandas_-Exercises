77f2ad5d497bb59b4cc45a8b1d00e3557850f06b
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-18 22:56:49 +0530

    Create Test1.csv file without index

---
+++ 33_problem.py
@@ -0,0 +1,6 @@
+# Write csv file without index
+import pandas as pd
+dic = {1:[1,2,3,4,5,6] , 2:[9,8,7,6,5,4], 3:[11,22,44,33,55,66]}
+S = pd.DataFrame(dic)
+print(S)
+S.to_csv("Test1.csv",index=False)
