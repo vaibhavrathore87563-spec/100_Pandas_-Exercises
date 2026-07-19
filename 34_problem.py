619f4e181849189ddf970c76da3d8f98fdae9370
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-19 11:50:04 +0530

    Access particular coloum USE->usecols[2]

--- 34_problem.py
+++ 34_problem.py
@@ -1,3 +1,4 @@
+# Write csv file without index and change column names
 import pandas as pd
 dic = {1:[1,2,3,4,5,6], 2:[9,8,7,6,5,4] , 3:[11,22,33,44,55,66]}
 S = pd.DataFrame(dic)
--- 36_problem.py
+++ 36_problem.py
@@ -1,3 +1,4 @@
+#Read file and access particular Row
 import pandas as pd
 read_students_csv = pd.read_csv("students.csv",nrows=5)
 print(read_students_csv)
