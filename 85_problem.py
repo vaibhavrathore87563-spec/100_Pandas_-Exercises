0f8806156d51ae190a45d0f5d67e64584669a7ab
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-21 16:42:37 +0530

    Merge USE->how=outer

---
+++ 85_problem.py
@@ -0,0 +1,6 @@
+#How to merge two DataFrame USE->how="outer"
+
+import pandas as pd
+var1 = pd.DataFrame({"A":[1,2,3,4,5],"B":[11,12,13,14,15]})
+var2 = pd.DataFrame({"A":[2,3,4,5,7],"C":[10,14,15,16,11]})
+print(pd.merge(var1,var2,how="outer"))
