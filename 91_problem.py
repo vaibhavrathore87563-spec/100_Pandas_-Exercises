2b5bd025e447cd4332321abb47eb06f4209895ee
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-21 22:23:48 +0530

    Concat axis= , join=

---
+++ 91_problem.py
@@ -0,0 +1,5 @@
+# concat two Dataframes with axis
+import pandas as pd
+var1 = pd.DataFrame({"A":[1,2,3,4,5],"B":[11,12,13,14,15]})
+var2 = pd.DataFrame({"A":[2,3,4,5,7],"B":[10,14,15,16,11]})
+print(pd.concat([var1,var2],axis=1))
