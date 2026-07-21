7f3c46b277d15323fee7cda3f826c5000ddb97a4
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-21 16:55:28 +0530

    Merge same column dataframe and print

---
+++ 87_problem.py
@@ -0,0 +1,8 @@
+# Merge two same Column name dataframe and print this merge dataframe
+
+import pandas as pd
+
+var1 = pd.DataFrame({"A":[1,2,3,4,5],"B":[11,12,13,14,15]})
+
+var2 = pd.DataFrame({"A":[2,3,4,5,7],"B":[10,14,15,16,11]})
+print(pd.merge(var1,var2,left_index=True,right_index=True))
