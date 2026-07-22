3c710038be5b35c1d2a44269c2350511f0921303
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-22 21:47:20 +0530

    Reshape the DataFrame

---
+++ 106_problem.py
@@ -0,0 +1,4 @@
+# Reshape the dataframes USE-> melt fnxn change value column name
+import pandas as pd
+var = pd.DataFrame({"days":[1,2,3,4,5,6],"eng":[10,12,14,16,10,18],"maths":[17,12,32,45,76,79]})
+print(pd.melt(var,id_vars="days",var_name="python",value_name="Digits"))
