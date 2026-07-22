13877eedd8ff09691e88ab1ea8f1d06761e44af6
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-07-22 21:39:53 +0530

    Reshape DataFrame USE->melt and change cloumn name

--- 104_problem.py
+++ 104_problem.py
@@ -1,4 +1,4 @@
-# Reshape the dataframes USE-> melt fnxn change column name
+# Reshape the dataframes USE-> melt fnxn using one column as id variable
 import pandas as pd
 var = pd.DataFrame({"days":[1,2,3,4,5,6],"eng":[10,12,14,16,10,18],"maths":[17,12,32,45,76,79]})
-print(pd.melt(var,id_vars="days",var_name="python"))
+print(pd.melt(var,id_vars=["days"]))
